9671fcfff7c15464c8e9635b80ef9bd2478038c9
jagadishb1409 <jagadishbhilebhavi2701@gmail.com>
2023-01-22 00:35:46 +0530

    Add indentation.py

---
+++ indentation/indentation.py
@@ -0,0 +1,11 @@
+# Indentation refers to the spaces at the beginning of a code line.
+# Python uses indentation to indicate a block of code.
+
+#example:
+
+for i in range(5):
+    print(i)
+
+# here you can see I have used : instead of {} how it is used in other languages
+# once condition for loops is given we start writing with four space or tab space
+# please read PEP guidlines mentioned in the indentation.md
