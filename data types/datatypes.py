e3c120fc4fde0051bdabb91d96d07e68d0fafad8
jagadishb1409 <jagadishbhilebhavi2701@gmail.com>
2023-01-24 10:15:35 +0530

    Add Data types

---
+++ data types/datatypes.py
@@ -0,0 +1,15 @@
+x = 5    # integer
+y = 3.14 # floating-point number
+
+name = "John Smith" # string enclosed in double quotes
+address = 'New York'  # string enclosed in single quotes
+
+fruits = ["apple", "banana", "orange"] # list
+numbers = (1, 2, 3) # tuple
+
+person = {'name': 'John Smith', 'age': 30, 'address': 'New York'} # dictionary
+
+is_raining = False
+if is_raining:
+    print("Bring an umbrella!")
+
