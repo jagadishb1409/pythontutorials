aa5a361ad03e2873e5a2b9427e2c156ec3b72b15
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-02-18 22:32:50 +0530

    Update sets.py

--- 6. Data Structures/Working with Sets/sets.py
+++ 6. Data Structures/Working with Sets/sets.py
@@ -1,1 +1,44 @@
+my_set = set([1, 2, 3, 4, 5])
+
+my_set = {1, 2, 3, 4, 5}
+
+my_set.add(6)
+print(my_set)  # {1, 2, 3, 4, 5, 6}
+
+
+my_set.remove(3)
+print(my_set)  # {1, 2, 4, 5}
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union_set = set1.union(set2)
+print(union_set)  # {1, 2, 3, 4, 5}
+
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union_set = set1 | set2
+print(union_set)  # {1, 2, 3, 4, 5}
+
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+intersection_set = set1.intersection(set2)
+print(intersection_set)  # {3}
+
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+intersection_set = set1 & set2
+print(intersection_set)  # {3}
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+difference_set = set1.difference(set2)
+print(difference_set)  # {1, 2}
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+difference_set = set1 - set2
+print(difference_set)  # {1, 2}
 
