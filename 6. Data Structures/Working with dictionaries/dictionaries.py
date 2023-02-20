6efb0304a4ca45ae6b8909b31de130bd236a9f39
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-02-20 22:36:19 +0530

    Update dictionaries.py

--- 6. Data Structures/Working with dictionaries/dictionaries.py
+++ 6. Data Structures/Working with dictionaries/dictionaries.py
@@ -1,1 +1,13 @@
+employee_ages = {"John": 35, "Emily": 28, "Tom": 42}
 
+print(employee_ages["Emily"]) # Output: 28
+
+employee_ages["Kate"] = 29
+
+del employee_ages["Tom"]
+
+for name in employee_ages:
+    print(name, employee_ages[name])
+
+ for name, age in employee_ages.items():
+    print(name, age)
