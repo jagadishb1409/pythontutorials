1f704e89c4f31a4df8d02ba230c133779f35e97a
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-02-23 23:21:51 +0530

    Update main.py

--- 10. Using Python and Paramiko to Log in to a Server and Execute Basic Commands/main.py
+++ 10. Using Python and Paramiko to Log in to a Server and Execute Basic Commands/main.py
@@ -1,1 +1,18 @@
 
+import paramiko
+
+# create an SSH client
+ssh = paramiko.SSHClient()
+
+# automatically add the server's key
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# connect to the server
+ssh.connect('example.com', username='username', password='password')
+
+# execute a command
+stdin, stdout, stderr = ssh.exec_command('ls')
+print(stdout.readlines())
+
+# close the connection
+ssh.close()
