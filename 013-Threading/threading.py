b2be7e85bb5e84b1e32b0e5189bbba9f2d0384ee
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-03-04 22:39:30 +0530

    Update threading.py

--- 013-Threading/threading.py
+++ 013-Threading/threading.py
@@ -1,1 +1,17 @@
+import threading
+
+class MyThread(threading.Thread):
+    def run(self):
+        # Code to be executed in this thread
+        print("Hello from thread", self.name)
+
+        
+
+my_thread = MyThread()
+my_thread.start()
+
+
+my_thread = MyThread()
+my_thread.start()
+my_thread.join()
 
