cbe00cf782ccce835494b0593b96478c1d99806a
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-03-06 23:48:07 +0530

    Update threading.py

--- 013-Threading/threading.py
+++ 013-Threading/threading.py
@@ -15,3 +15,30 @@ my_thread = MyThread()
 my_thread.start()
 my_thread.join()
 
+
+import threading
+
+# Create a semaphore object with a maximum of 3 threads allowed to access at a time
+semaphore = threading.Semaphore(3)
+
+def worker():
+    """Thread worker function"""
+    semaphore.acquire()
+    try:
+        # Do some work...
+        print(f"Thread {threading.current_thread().name} is working")
+    finally:
+        semaphore.release()
+
+# Create 10 worker threads
+threads = [threading.Thread(target=worker, name=f"Thread-{i}") for i in range(10)]
+
+# Start the threads
+for t in threads:
+    t.start()
+
+# Wait for the threads to finish
+for t in threads:
+    t.join()
+
+print("All threads finished")
