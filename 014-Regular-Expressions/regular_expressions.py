2c5c729f17e87df3214c25e5d5534f0c24645e21
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-03-08 23:34:04 +0530

    Update regular_expressions.py

--- 014-Regular-Expressions/regular_expressions.py
+++ 014-Regular-Expressions/regular_expressions.py
@@ -1,1 +1,25 @@
 
+import re
+
+text = "Hello, world!"
+pattern = "hello"
+
+if re.search(pattern, text, re.IGNORECASE):
+    print("Match found")
+else:
+    print("Match not found")
+    
+
+text = "Call me at (123) 456-7890 or (987) 654-3210"
+pattern = r"\(\d{3}\) \d{3}-\d{4}"
+
+matches = re.findall(pattern, text)
+print(matches)
+
+
+text = "The color of the sky is blue"
+pattern = "color"
+replacement = "colour"
+
+new_text = re.sub(pattern, replacement, text)
+print(new_text)
