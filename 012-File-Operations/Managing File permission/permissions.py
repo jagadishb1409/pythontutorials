ae73c3f6bbc70460d82d63a45afdd3967ffcef60
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-03-23 23:54:22 +0530

    Update permissions.py

--- 012-File-Operations/Managing File permission/permissions.py
+++ 012-File-Operations/Managing File permission/permissions.py
@@ -1,1 +1,13 @@
 
+import os
+
+# Get file permissions
+permissions = oct(os.stat("example.txt").st_mode)[-3:]
+
+# Print file permissions
+print("File permissions:", permissions)
+
+
+# Set file permissions to read-only
+os.chmod("example.txt", 0o400)
+
