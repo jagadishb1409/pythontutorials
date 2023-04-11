e53f06f44e527ec7762649c33681543f0f5c728d
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-04-11 22:53:23 +0530

    Update pandas-example.py

--- 019-Python-Modules-in-detail/Pandas/pandas-example.py
+++ 019-Python-Modules-in-detail/Pandas/pandas-example.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+# Set the API endpoint URL for Bitcoin
+url = 'https://api.coingecko.com/api/v3/coins/bitcoin'
+
+# Fetch data from the API
+response = requests.get(url)
+
+# Convert the response to a JSON object
+data = response.json()
+
+# Create a Pandas DataFrame from the JSON data
+df = pd.json_normalize(data)
+
+# Save the DataFrame as a CSV file
+df.to_csv('bitcoin_data.csv', index=False)
+
+# Print a success message
+print('Bitcoin data saved to bitcoin_data.csv')
