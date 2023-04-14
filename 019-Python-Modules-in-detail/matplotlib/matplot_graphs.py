9232897ade0dab6f68276cd941a291257f00ca94
jagadish bilebhavi <63513035+jagadishb1409@users.noreply.github.com>
2023-04-14 23:42:46 +0530

    Update matplot_graphs.py

--- 019-Python-Modules-in-detail/matplotlib/matplot_graphs.py
+++ 019-Python-Modules-in-detail/matplotlib/matplot_graphs.py
@@ -1,1 +1,12 @@
 
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 8, 6, 4, 2]
+
+plt.plot(x, y)
+plt.title("Simple Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.legend(["Y-values"])
+plt.show()
